737fa3835c244dd93375442795d425fef44d66c8
Jan Wieczorek <info@kampagne-online.de>
2022-04-09 12:04:19 +0200

    updates circle ui

--- src/components/views/Dashboard/ExpensesCircle.vue
+++ src/components/views/Dashboard/ExpensesCircle.vue
@@ -4,17 +4,21 @@
     >
         <vue-ellipse-progress
             :key="ellipseKey"
-            :progress="50"
+            :progress="ellipseProgress"
             :legend="false"
             :loading="false"
+            :color="progressColor"
             :size="ellipseSize"
         >
             <div slot="legend-caption">
-                <span class="display-1 green--text text--lighten-1">
+                <span
+                    class="green--text text--lighten-1"
+                    :class="textSize.title"
+                >
                     {{ moneyEarned }}
                 </span>
                 <v-spacer />
-                <span class="subtitle-1 red--text text--lighten-1">
+                <span class="subtitle-1 red--text text--lighten-1" :class="textSize.subtitle">
                     {{ moneySpend }}
                 </span>
             </div>
@@ -24,6 +28,7 @@
 
 <script>
 
+const textBreakpoint = 650;
 const ellipseLimits = {
     min: 450,
     max: 800
@@ -38,6 +43,10 @@ export default {
             window: {
                 width: 0,
                 height: 0
+            },
+            money: {
+                earned: 500.00,
+                spend: 375.00
             }
         };
     },
@@ -45,12 +54,17 @@ export default {
         moneyEarned()
         {
             return new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'})
-                .format(500.20);
+                .format(this.money.earned);
         },
         moneySpend()
         {
             return new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'})
-                .format(200.32);
+                .format(this.money.spend);
+        },
+        ellipseProgress()
+        {
+            const {earned, spend} = this.money;
+            return (spend / earned) * 100;
         },
         ellipseSize()
         {
@@ -60,17 +74,40 @@ export default {
             width = (width > ellipseLimits.max) ? ellipseLimits.max : width;
 
             return width * 0.8;
+        },
+        textSize()
+        {
+            const {width} = this.window;
+
+            return {
+                title: width > textBreakpoint ? 'text-h1' : 'text-h2',
+                subtitle: width > textBreakpoint ? 'text-h3' : 'text-h4'
+            };
+        },
+        balancePositive()
+        {
+            const {earned, spend} = this.money;
+            return earned > spend;
+        },
+        progressColor()
+        {
+            return this.balancePositive ? '#4caf50' : '#f44336';
         }
     },
     mounted()
     {
         window.addEventListener('resize', this.handleResize);
+        this.handleResize();
     },
     methods: {
         handleResize()
         {
             this.window.width = window.innerWidth;
             this.window.height = window.innerHeight;
+            this.reRenderProgress();
+        },
+        reRenderProgress()
+        {
             this.ellipseKey++;
         }
     }
